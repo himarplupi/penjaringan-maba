13e0a4f80d77730f40aabe32d88d0b7bf5e7fd05
AkbarMahmudin <wastanami17@gmail.com>
2022-06-23 10:16:01 +0700

    resolve port

--- app.js
+++ app.js
@@ -115,4 +115,4 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
-server.listen(3000, () => console.log(`Listen : http://localhost:3000`));
+server.listen(process.env.PORT || 3000);
